01bd61fce14305cdcd3e005af9ae38bf52ac6997
Eth-H <emerald147568@gmail.com>
2019-09-24 10:11:56 +0100

    files

--- Methods/Linux/Scripts/install scripts/arch_install_script.sh
+++ Methods/Linux/Scripts/install scripts/arch_install_script.sh
@@ -9,3 +9,16 @@
 		cd yaourt/ && makepkg -si
 #screen snipper, uses less dependancies than shutter
 pacman -S screenFetch	
+
+#virtualisation
+#virtual box
+pacman install virtualbox $(pacman -Qsq "^linux" | grep "^linux[0-9]*[-rt]*$" | awk '{print $1"-virtualbox-host-modules"}' ORS=' ')
+#add virtual box module to kernel, if further problems https://wiki.manjaro.org/index.php?title=VirtualBox
+sudo vboxreload
+sudo gpasswd -a $USER vboxusers
+#virtualbox guest additions
+pamac install virtualbox-guest-utils $(pacman -Qsq "^linux" | grep "^linux[0-9]*[-rt]*$" | awk '{print $1"-virtualbox-guest-modules"}' ORS=' ')#pacman -S virtualbox-guest-dkms
+sudo modprobe vboxguest vboxvideo vboxsf
+sudo gpasswd -a $USER vboxsf
+sudo systemctl enable --now vboxservice.service
+#sudo rcvboxdrv
